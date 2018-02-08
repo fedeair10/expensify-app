0e21313b0ffc50a30c74bace301242c6801215a3
Federico Sandri <federico.sandri@usablenet.com>
2018-02-08 18:15:03 +0100

    setup remove and edit expenses

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,15 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import {
+	startAddExpense,
+	addExpense,
+	editExpense,
+	removeExpense,
+	setExpenses,
+	startSetExpenses,
+	startRemoveExpense,
+	startEditExpense
+		} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -27,6 +36,22 @@ test('should setup remove expense action object', () => {
 	});
 });
 
+test('should remove expense from firebase', (done) => {
+	const store = createMockStore({});
+	const id = expenses[2].id;
+	store.dispatch(startRemoveExpense({ id })).then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'REMOVE_EXPENSE',
+			id
+		});
+		return database.ref(`expenses/${id}`).once('value');
+	}).then((snapshot) => {
+		expect(snapshot.val()).toBeFalsy();
+		done();
+	});
+});
+
 test('should setup edit expense action object', () => {
 	const action = editExpense('123abc',{ note: 'New note value' });
 	expect(action).toEqual({
@@ -35,6 +60,26 @@ test('should setup edit expense action object', () => {
 		updates: {
 			note: 'New note value'
 		}
+	});
+});
+
+test('should edit expense from firebase', (done) => {
+	const store = createMockStore({});
+	const id = expenses[2].id;
+	const updates = {
+		'note' : 'new note value updated'
+	}
+	store.dispatch(startEditExpense(id, updates)).then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'EDIT_EXPENSE',
+			id,
+			updates
+		});
+		return database.ref(`expenses/${id}`).once('value');
+	}).then((snapshot) => {
+		expect(snapshot.val().note).toBe(updates.note);
+		done();
 	});
 });
 
